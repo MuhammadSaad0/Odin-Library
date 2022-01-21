ca3704f2d67cd5eaeb702206ed0b5f2a72590eb4
unknown <muhammadsaadz100@gmail.com>
2022-01-22 00:37:58 +0500

    Add Book addition functionality

--- script.js
+++ script.js
@@ -19,10 +19,18 @@ function addBookToLibrary() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+    let read = document.querySelector("input[name = readstatus]:checked").value;
     let newbook = new Book(title, author, pages, read);
     myLibrary.push(newbook);
+    document.getElementById("addingform").style.visibility = "hidden";
+    document.getElementById("add").style.visibility = "visible";
     displaybooks();
 }
+function displayform() {
+    document.getElementById("addingform").style.visibility = "visible";
+    document.getElementById("add").style.visibility = "hidden";
+}
 let add = document.querySelector("#add");
-add.addEventListener("click", addBookToLibrary);
+add.addEventListener("click", displayform);
+let add2 = document.querySelector("#addingdone");
+add2.addEventListener("click", addBookToLibrary);
