7d85e95ad9838c3ad44de13cf14bf5501e368ffa
unknown <muhammadsaadz100@gmail.com>
2022-01-28 01:57:54 +0500

    Fix Remove button

--- script.js
+++ script.js
@@ -8,18 +8,34 @@ function Book(title, author, pages, read) {
         return title + " " + author + " " + pages + " " + read;
     }
 }
-function appendCardDiv(title, author, pages, read) {
+
+function appendCardDiv() {
     let div = document.getElementById("cards");
     let newcard = document.createElement("div");
-    newcard.innerHTML = title + "<br><br>" + author + "<br><br>" + pages + "<br><br>" + read;
-    newcard.classList.add("card");
-    div.appendChild(newcard);
+    for (let i = 0; i < myLibrary.length; i++) {
+        newcard.innerHTML = myLibrary[i].title + "<br><br>" + myLibrary[i].author + "<br><br>" + myLibrary[i].pages + "<br><br>" + myLibrary[i].read + "<br><br><button type = 'button' id = 'remove' value = '" + i + "' =>Remove</button>";
+        newcard.classList.add("card");
+        div.appendChild(newcard);
+    }
+    let remove = document.querySelectorAll("#remove");
+    for (let j = 0; j < remove.length; j++) {
+        remove[j].addEventListener("click", function () {
+            let index = remove[j].value;
+            myLibrary.splice(index, 1);
+            clearAll();
+            appendCardDiv();
+        });
+    }
 }
 function clearInputForms() {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
     document.getElementById("pages").value = "";
     document.querySelector("input[name = readstatus]:checked").checked = false;
+}
+function clearAll() {
+    let div = document.getElementById("cards");
+    div.innerHTML = "";
 }
 function addBookToLibrary() {
     let title = document.getElementById("title").value;
@@ -37,6 +53,7 @@ function displayform() {
     document.getElementById("addingform").style.visibility = "visible";
     document.getElementById("add").style.visibility = "hidden";
 }
+
 let add = document.querySelector("#add");
 add.addEventListener("click", displayform);
 let add2 = document.querySelector("#addingdone");
